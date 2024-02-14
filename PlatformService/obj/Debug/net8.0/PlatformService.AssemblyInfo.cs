36c27ed31e6099d7478e18c2e0521ba855a0b7d6
Joydip <joydip.mondal@gmail.com>
2024-02-14 17:36:22 +0530

    yaml update

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4243b0d5e91ba0445534fec28ae713322f854250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e70b1d854198396f831f90813f6a9965da315ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
